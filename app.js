00f1afe0995c033dd0b1cb03c8f4cc887e5e99db
transfusionbb-cnmi <transfusionbb_cnmi@mahidol.ac.th>
2026-09-06 08:11:32 +0700

    Add files via upload
    cnmi_donor_supabase_app_v13_1

--- app.js
+++ app.js
@@ -2034,7 +2034,7 @@ function initPwaShell() {
 
   if ("serviceWorker" in navigator && location.protocol === "https:") {
     window.addEventListener("load", function() {
-      navigator.serviceWorker.register("service-worker.js?v=13").catch(function(err) {
+      navigator.serviceWorker.register("service-worker.js?v=13.1").catch(function(err) {
         console.warn("Service worker registration failed", err);
       });
     });
--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "cnmi-donor-v13";
+const CACHE_NAME = "cnmi-donor-v13-1";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=13",
-  "./app.js?v=13",
-  "./config.js?v=13",
+  "./styles.css?v=13.1",
+  "./app.js?v=13.1",
+  "./config.js?v=13.1",
   "./manifest.webmanifest",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
